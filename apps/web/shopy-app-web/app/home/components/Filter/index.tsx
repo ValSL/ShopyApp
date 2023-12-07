b04df2c0c36770d4696ea2fa15ca8e149f5cd07f
ValSL <valid.saley@gmail.com>
2023-12-07 14:01:12 +0300

    products card section UI

--- apps/web/shopy-app-web/app/home/components/Filter/index.tsx
+++ apps/web/shopy-app-web/app/home/components/Filter/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import classes from "./filter.module.css";
 import { Box, Flex, NumberInput, SimpleGrid, Text, rem } from "@mantine/core";
-import { IconX, IconCurrencyDram  } from "@tabler/icons-react";
+import { IconX, IconCurrencyDram } from "@tabler/icons-react";
 
 const Filter = () => {
-    const icon = <IconCurrencyDram style={{ width: rem(0), height: rem(0) }} stroke={0} />;
-    const from = <Text size="14px">From:</Text>;
+	const icon = <IconCurrencyDram style={{ width: rem(0), height: rem(0) }} stroke={0} />;
+	const from = <Text size="14px">From:</Text>;
+	const to = <Text size="14px">To:</Text>;
 	return (
 		<Box className={classes.filterbox}>
 			<SimpleGrid cols={2} spacing={0} className={classes.container}>
@@ -13,17 +14,29 @@ const Filter = () => {
 					Filters
 				</Text>
 				<Flex align="center" className={classes.resetButton} gap={5}>
-					<Text size="14px">
-						Reset
-					</Text>
-					<IconX size={14}/>
+					<Text size="14px">Reset</Text>
+					<IconX size={14} />
 				</Flex>
 
 				<Box className={classes.filterContentBox2}>
-					<Text fw="bold" size="16px">Price</Text>
+					<Text fw="bold" size="16px">
+						Price
+					</Text>
 				</Box>
-				<NumberInput styles={{section: {marginLeft: '10px'}, input: {paddingLeft: '50px'}}} prefix="$" className={classes.moneyFilter} leftSection={from} rightSection={icon}/>
-				<NumberInput styles={{section: {marginLeft: '10px'}, input: {paddingLeft: '50px'}}} prefix="$" className={classes.moneyFilter} leftSection={from} rightSection={icon}/>
+				<NumberInput
+					styles={{ section: { marginLeft: "10px" }, input: { paddingLeft: "47px" } }}
+					prefix="$"
+					className={classes.moneyFilter}
+					leftSection={from}
+					rightSection={icon}
+				/>
+				<NumberInput
+					styles={{ section: { marginLeft: "2px" }, input: { paddingLeft: "32px" } }}
+					prefix="$"
+					className={classes.moneyFilter}
+					leftSection={to}
+					rightSection={icon}
+				/>
 			</SimpleGrid>
 		</Box>
 	);
--- apps/web/shopy-app-web/app/home/page.tsx
+++ apps/web/shopy-app-web/app/home/page.tsx
@@ -1,15 +1,74 @@
-import { Box, SimpleGrid, Container } from "@mantine/core";
+import { Box, SimpleGrid, Container, TextInput, Text, CardSection, Image, Flex, Card, Button } from "@mantine/core";
 import React from "react";
 import Header from "../components/Header";
 import Filter from "./components/Filter";
+import classes from "./home.module.css";
+import { IconSearch } from "@tabler/icons-react";
+
+const Cardd = () => {
+	return (
+		<Card p={0} className={classes.card} >
+			<CardSection>
+				<Image
+					height={160}
+					src="https://firebasestorage.googleapis.com/v0/b/myshopyapp.appspot.com/o/images%2Fflowers.jpeg07b092a3-964f-41fc-bfa3-b03d1f0f0ad8?alt=media&token=e533d6f2-acdb-41da-acda-22c96533fcf6"
+					alt="Norway"
+				/>
+			</CardSection>
+			<Flex p={15} direction="column" gap="12px">
+				<Text fw="bold" size="20px">
+					Title
+				</Text>
+				<Flex justify="space-between">
+					<Text size="14px" c="#A3A3A3">
+						Price:
+					</Text>
+					<Text fw="bold" size="20px">
+						$123
+					</Text>
+				</Flex>
+				<Button>Add to Cart</Button>
+			</Flex>
+		</Card>
+	);
+};
 
 const Home = () => {
 	return (
-		<SimpleGrid cols={4} px="3rem" >
-			<Box><Filter/></Box>
-			<div>2</div>
-			<div>3</div>
-			<div>4</div>
+		<SimpleGrid cols={4} px="3rem" spacing={30}>
+			<Box className={classes.filter}>
+				<Filter />
+			</Box>
+			<Box className={classes.searchInput}>
+				<TextInput leftSection={<IconSearch size={16} />} radius="8px" placeholder="Type to search..." />
+			</Box>
+			<Box>
+				<Text>12 Results</Text>
+			</Box>
+			<Box style={{ justifySelf: "end", gridColumn: "3 / span 2" }}>
+				<Text>Sort by newest</Text>
+			</Box>
+			<Box style={{ justifySelf: "start", gridColumn: "2 / span 3" }}>
+				<Text>400 - 500</Text>
+			</Box>
+			<div>
+				<Cardd />
+			</div>
+			<div>
+				<Cardd />
+			</div>
+			<div>
+				<Cardd />
+			</div>
+			<div>
+				<Cardd />
+			</div>
+			<div>
+				<Cardd />
+			</div>
+			<div>
+				<Cardd />
+			</div>
 		</SimpleGrid>
 	);
 };
